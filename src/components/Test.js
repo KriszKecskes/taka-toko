5694d26af3ff3514e76dbac7045bd6e215e0bb9f
Krisztian Kecskes <kecsk.krisztian@gmail.com>
2019-01-14 14:09:56 +0100

    master | Add CSS support in JS & Add async/await support

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,11 +1,10 @@
 import React, { PureComponent } from 'react';
+import './Test.css';
 
 export default class extends PureComponent {
-
   render() {
     return(
-      <div>This is a Test! :-)</div>
+      <div className="test">This is a Test! :-)</div>
     )
   }
-
 };
